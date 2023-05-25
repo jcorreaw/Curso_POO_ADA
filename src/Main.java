4b7f41b94c130302d7e2364c7ea9c60b9e3992fd
jcorreaw <juninhocorrea2014@gmail.com>
2023-05-25 15:51:32 -0300

    Criando a classe e o objeto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import Animais.Cachorro;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cachorro cachorro1 = new Cachorro();
+        cachorro1.nome = "Puppy";
+        cachorro1.cor = "Marrom";
+        cachorro1.altura = 25;
+        cachorro1.peso = 5.5;
+        cachorro1.tamanhoDoRabo = 5;
+        System.out.println(cachorro1);
     }
 }
