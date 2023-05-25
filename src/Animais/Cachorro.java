a2f57eed7f1591223cf37585abd61a8ef94f8846
jcorreaw <juninhocorrea2014@gmail.com>
2023-05-25 16:03:59 -0300

    Metodos

--- src/Animais/Cachorro.java
+++ src/Animais/Cachorro.java
@@ -6,4 +6,27 @@ public class Cachorro {
    public int altura;
    public double peso;
    public int tamanhoDoRabo;
+   public String estadoDeEspirito;
+
+   public void comer(){}
+
+   public void latir(){
+       System.out.println("AU AU");
+   }
+   public String pegar(){
+       return "Bolinha";
+   }
+
+   public String interagir(String acao){
+       if (acao.equalsIgnoreCase("Carinho")){
+           this.estadoDeEspirito = "Feliz";
+       }else if (acao.equalsIgnoreCase("Vai Dormir")){
+           this.estadoDeEspirito = "Bravo";
+       }
+       else {
+           this.estadoDeEspirito = "Neutro";
+       }
+       return estadoDeEspirito;
+   }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,12 @@ public class Main {
         cachorro1.altura = 25;
         cachorro1.peso = 5.5;
         cachorro1.tamanhoDoRabo = 5;
-        System.out.println(cachorro1);
+
+        cachorro1.latir();
+        System.out.println("O cachorro pegou uma " + cachorro1.pegar());
+        System.out.println("O cachoro está " + cachorro1.interagir("Carinho"));
+        System.out.println("O cachoro está " + cachorro1.interagir("Nada"));
+        System.out.println("O cachoro está " + cachorro1.interagir("Vai Dormir"));
+
     }
 }
