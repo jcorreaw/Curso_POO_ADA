84adc683f3a0b5f4cd2fdc3f6e40d428ae8eb03b
jcorreaw <juninhocorrea2014@gmail.com>
2023-05-25 18:18:25 -0300

    Encapsulamento

--- src/Animais/Cachorro.java
+++ src/Animais/Cachorro.java
@@ -1,12 +1,68 @@
 package Animais;
 
 public class Cachorro {
-    public String nome;
-    public String cor;
-    public int altura;
-    public double peso;
-    public int tamanhoDoRabo;
-    public String estadoDeEspirito;
+    private String nome;
+    private String cor;
+    private int altura;
+    private double peso;
+    private int tamanhoDoRabo;
+    private String estadoDeEspirito;
+
+    public Cachorro() {
+
+    }
+
+    public Cachorro(String nome, String cor, int altura, double peso, int tamanhoDoRabo) {
+        this.nome = nome;
+        this.cor = cor;
+        this.altura = altura;
+        this.peso = peso;
+        this.tamanhoDoRabo = tamanhoDoRabo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getTamanhoDoRabo() {
+        return tamanhoDoRabo;
+    }
+
+    public void setTamanhoDoRabo(int tamanhoDoRabo) {
+        this.tamanhoDoRabo = tamanhoDoRabo;
+    }
+
+    public String getEstadoDeEspirito() {
+        return estadoDeEspirito;
+    }
 
     public void comer() {
     }
